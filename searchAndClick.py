2070756082d1657710227046f290aad99a7d88c4
Subash Gautam <subashgautam55@gmail.com>
2023-07-24 20:37:58 +0545

    testfile creating and practice basics of python

--- searchAndClick.py
+++ searchAndClick.py
@@ -6,19 +6,30 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 
-driver.get("https://coerver.draftserver.com/players-club/")
+driver.get("https://astrareviews.com/")
 driver.maximize_window()
 
-search = driver.find_element(By.XPATH, "//input[@id = 'course-text']" ).send_keys("Skills" + Keys.ENTER)
+# HEADER AND SEARCH BAR CASES
+search = driver.find_element(By.CLASS_NAME, "search-toggle")
+search = driver.find_element(By.TAG_NAME, "input").send_keys("rich")
+search = driver.find_element(By.CLASS_NAME, "search-field").send_keys("rich"+ Keys.ENTER)
+search = driver.implicitly_wait(20)
+time.sleep(20)
+
+
+search = driver.find_element(By.XPATH, "//input[@class = 'course-text']" ).send_keys("Skills" + Keys.ENTER)
 time.sleep(5)
 
 search = driver.find_element(By.XPATH, "//input[@id = 'course-text']" ).clear()
 
-# first_item = driver.find_element(By.CSS_SELECTOR, ".rt-tbody[]")
-# first_item.click()
+first_item = driver.find_element(By.CSS_SELECTOR, ".rt-tbody[]")
+first_item.click()
 
-# login_item = driver.find_element(By.ID, "login")
-# login_item.click()
+login_item = driver.find_element(By.ID, "blossomthemes-email-newsletter-114")
+login_item.click()
 
-time.sleep(20)
+login_item = driver.find_element(By.XPATH, "//input[@id = 'blossomthemes-email-newsletter-114']" ).send_keys("testemail@sel.com" + Keys.ENTER)
+login_item = driver.find_element(By.XPATH, "//input[@class = 'subscribe-submit-114']").click()
+
+time.sleep(2000)
 driver.quit()
---
+++ testFile.py
@@ -0,0 +1,9 @@
+nepal = "meroDesh"
+print(nepal)
+
+a,d,r = 5, 7, "hello again"
+b = 9
+c = d + b
+print (c,d,r)
+
+print("{} {} {}".format("Oh hi",r,a))
