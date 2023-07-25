f95ca2d4e3f78675a9bdbcdf9997952ab6a27843
Subash Gautam <subashgautam55@gmail.com>
2023-07-25 20:39:18 +0545

    python datatypes and format practice

--- testFile.py
+++ testFile.py
@@ -6,4 +6,15 @@ b = 9
 c = d + b
 print (c,d,r)
 
+#data types and print in different format
 print("{} {} {}".format("Oh hi",r,a))
+
+#what kind of data type
+print (type(r))
+
+values = [1,2,"Selenium", 9.9, 67]
+
+print(values[-1])
+print (type(values))
+print(values[1])
+print(values[2:5])
