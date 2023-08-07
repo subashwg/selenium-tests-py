af36eed116561204e228fc4be9c520250a6e22b7
Subash Gautam <subashgautam55@gmail.com>
2023-08-07 17:04:00 +0545

    examples on for loop

--- testFlowControl.py
+++ testFlowControl.py
@@ -13,10 +13,11 @@ else:
 #for loop condition
 
 numbers = [1,2,3,4,6,8,9]
-
 for i in numbers:
     print(i*2)
+print("*******************************")
 
+#syntax with first and last index
 #sum of first five natural numbers
 #range (i,j) -> i to j-1
 
@@ -25,3 +26,17 @@ for j in range(1,7):
     #print(j)
     summation = summation + j
     print(summation)
+print("*******************************")
+
+#syntax with range and specific iteration count to jump
+#programming examples(print numbers from 1 to 10 with summation)
+
+for k in range(1, 20, 4):
+    print(k)
+print ("********************")
+
+
+#syntax with only upper bound
+#printing numbers serially
+for m in range(20):
+    print(m)
