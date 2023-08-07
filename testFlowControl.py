91d50756cdb958d0f0791161f154403e71ae60da
Subash Gautam <subashgautam55@gmail.com>
2023-08-07 16:49:12 +0545

    basic for loop understanding in python

--- testFlowControl.py
+++ testFlowControl.py
@@ -8,3 +8,20 @@ if maths<56:
     print ("hola")
 else:
     print ("no, lo siento")
+
+
+#for loop condition
+
+numbers = [1,2,3,4,6,8,9]
+
+for i in numbers:
+    print(i*2)
+
+#sum of first five natural numbers
+#range (i,j) -> i to j-1
+
+summation = 10
+for j in range(1,7):
+    #print(j)
+    summation = summation + j
+    print(summation)
