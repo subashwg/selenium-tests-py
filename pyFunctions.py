e0d1eecaf68883cc2adce68e794a020f831c55bc
Subash Gautam <subashgautam55@gmail.com>
2023-08-10 17:51:44 +0545

    python functions

---
+++ pyFunctions.py
@@ -0,0 +1,15 @@
+# function is group of related statements that perform a specific task
+
+#function declaration and calling in python
+def Hello(name):
+    print("Hola! Mucho Gusto" + name)
+
+def subtractInt(a,b):
+    print(a-b)
+
+def addInt(c,d):
+    return(c+d)
+
+Hello("JOHN")
+subtractInt(9,3)
+print(addInt(5,6))
